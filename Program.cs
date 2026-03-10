fa81c3cb7b1fadacdd50b88f8a7872b56defc168
zeyad tarek <vetogamer5@outlook.com>
2026-03-10 03:47:36 +0200

    Q2

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 using System.Numerics;
 using System.Runtime.Intrinsics.X86;
 using System.Security.Principal;
+using System.ComponentModel;
+using System.Security.Claims;
+using System.Xml.Linq;
 
 namespace ConsoleApp1
 {
@@ -13,25 +16,25 @@ namespace ConsoleApp1
             //Abstraction
 
             //Abstraction means hiding complex implementation details and showing only the essential features of an object.
-            
+
             //It focuses on what the object does, not how it does it.
-            
+
             //Abstraction is usually implemented using:
-            
+
             //Abstract classes
-            
+
             //Interfaces
-            
+
             //Encapsulation
-            
+
             //Encapsulation means wrapping data and methods together in a single unit(class) and controlling access to the data using access modifiers like:
-            
+
             //private
             //    protected
             //    public
-            
+
             //Encapsulation focuses on protecting data from direct access.
-            
+
             //Difference
             //Abstraction Encapsulation
             //Hides implementation details Hides internal data
@@ -39,27 +42,85 @@ namespace ConsoleApp1
             //Implemented using abstract classes and interfaces Implemented using access modifiers
             //Shows only necessary functionality Controls access to variables
             //Real World Example
-            
+
             //ATM Machine
-            
+
             //When using an ATM:
-            
+
             //You only see options like:
-            
+
             //Withdraw
-            
+
             //Deposit
-            
+
             //Check balance
-            
+
             //You don't see how the bank system processes transactions internally.
-            
+
             //That is Abstraction.
-            
+
             //Inside the ATM system, your account balance is private and can only be accessed through methods like Withdraw() or Deposit().
-            
+
             //That is Encapsulation.
             #endregion
-        }
+
+            #region Q2
+        //    Methods Implementation
+
+        //Abstract Class
+        
+        //Can contain abstract and normal methods
+        
+        //Interface
+        
+        //Contains method declarations only(until newer C# versions)
+        
+        //Fields / Variables
+        
+        //Abstract Class
+        
+        //Can have fields and properties.
+        
+        //Interface
+        
+        //Cannot have fields.
+        
+        //Constructors
+        
+        //Abstract Class
+        
+        //Can have constructors.
+        
+        //Interface
+        
+        //Cannot have constructors.
+        
+        //Inheritance
+        
+        //Abstract Class
+        
+        //A class can inherit only one abstract class
+        
+        //Interface
+        
+        //A class can implement multiple interfaces
+        
+        //When to use each?
+        
+        //Use Abstract Class when:
+        
+        //Classes share common behavior and code
+        
+        //You want some methods implemented
+        
+        //Use Interface when:
+        
+        //You want to define a contract
+        
+        //Multiple classes must implement the same behavior
+        
+        //You need multiple inheritance
+        #endregion
+    }
     }
 }
